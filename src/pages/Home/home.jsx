14162fd351f7b940107fec53bd81b4145a348044
vLamass <viniciusslamas17@gmail.com>
2026-06-08 14:19:57 -0300

    feat: Criação de Componentes, Carousel, DivisaoTitulo, Card e Home é adição de assets

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,13 @@
+import Carousel from "../../componets/Carousel/Carousel";
+import DivisaoTitulo from "../../componets/DivisaoTitulo/DivisaoTitulo"
+
+function Home() {
+  return (
+    <>
+      <Carousel />
+      <DivisaoTitulo titulo="Receitas em Destaque"/>
+    </>
+  );
+}
+
+export default Home;
